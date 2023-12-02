ec38bf841a739faf638ad716e6a5c76e5a04e11d
Ian Edwards <ian.philip.edwards@gmail.com>
2023-12-02 23:54:32 +0000

    main and led changes

--- led.py
+++ led.py
@@ -1,14 +1,14 @@
 import machine
 import time
-from status import Status
+
+def led_status_ok():
+    machine.Pin("LED", machine.Pin.OUT).on()
     
-def led_status(status_code):
-    machine_led = machine.Pin("LED", machine.Pin.OUT)
-    if status_code == Status.OK():
-        machine_led.on()
-    else:        
-        while (True):
-            machine_led.on()
-            time.sleep(0.5)
-            machine_led.off()
-            time.sleep(0.5)
+def led_status_error(error_code):
+    led = machine.Pin("LED", machine.Pin.OUT)
+    led.off()
+    while (True):
+        led.on()
+        time.sleep(0.5)
+        led.off()
+        time.sleep(0.5)
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import machine
-led = machine.Pin("LED", machine.Pin.OUT)
-led.off()
-led.on()
+from led import led_status_ok, led_status_error
+from wifi import wifi_connect
+from secrets import wifi_name, wifi_password
+import time
 
-print('hello world!')
+led_status_ok()
+wifi_connect(wifi_name, wifi_password)
+time.sleep(2)
+led_status_error(123)

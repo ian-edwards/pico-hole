1323feef41aff33dc94767780a0b95af71e92bc7
Ian Edwards <ian.philip.edwards@gmail.com>
2023-12-07 23:27:47 +0000

    added led on function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ from time import ticks_ms, ticks_add, ticks_diff
 
 def raise_exception(error_message): raise Exception(error_message)
 
+def led_on(): Pin("LED", Pin.OUT).on()
+
 def connect_wifi(wifi_name, wifi_password, wifi_timeout):
     deadline = ticks_add(ticks_ms(), wifi_timeout)
     wifi = WLAN(STA_IF)
@@ -13,7 +15,7 @@ def connect_wifi(wifi_name, wifi_password, wifi_timeout):
         pass
     return wifi.isconnected()
 
-Pin("LED", machine.Pin.OUT).on()
+led_on()
 connect_wifi(wifi_name="Your WiFi Name", wifi_password="Your WiFi Password", wifi_timeout=5_000) or raise_exception("Wifi Error!")
 
 print("WiFi OK!")

fc1d4f890003f6ae4a642b212e1f2d1fb5f0d82a
Ian Edwards <ian.philip.edwards@gmail.com>
2023-12-07 23:32:55 +0000

    improved connect_wifi

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from network import WLAN, STA_IF
-from time import ticks_ms, ticks_add, ticks_diff
+from time import ticks_ms, ticks_add, ticks_diff, sleep
 
 def raise_exception(error_message): raise Exception(error_message)
 
 def connect_wifi(wifi_name, wifi_password, wifi_timeout):
-    deadline = ticks_add(ticks_ms(), wifi_timeout)
+    deadline = ticks_add(ticks_ms(), max(wifi_timeout, 0))
     wifi = WLAN(STA_IF)
     wifi.active(True)
     wifi.connect(wifi_name, wifi_password)
     while (wifi.isconnected() == False and ticks_diff(deadline, ticks_ms()) > 0):
-        pass
+        sleep(0.5)
     return wifi.isconnected()
 
 connect_wifi(wifi_name="Your WiFi Name", wifi_password="Your WiFi Password", wifi_timeout=5_000) or raise_exception("Wifi Error!")

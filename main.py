6767df7aaf2b55b3632d627ff7b132d301d87388
Ian Edwards <ian.philip.edwards@gmail.com>
2023-12-07 23:34:36 +0000

    refactor connect_wifi

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from time import ticks_ms, ticks_add, ticks_diff, sleep
 def raise_exception(error_message): raise Exception(error_message)
 
 def connect_wifi(wifi_name, wifi_password, wifi_timeout):
-    deadline = ticks_add(ticks_ms(), max(wifi_timeout, 0))
     wifi = WLAN(STA_IF)
     wifi.active(True)
     wifi.connect(wifi_name, wifi_password)
+    deadline = ticks_add(ticks_ms(), max(wifi_timeout, 0))
     while (wifi.isconnected() == False and ticks_diff(deadline, ticks_ms()) > 0):
         sleep(0.5)
     return wifi.isconnected()
